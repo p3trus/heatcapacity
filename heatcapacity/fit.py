b7aa167e55e49bccb15e94cee83a894fefa7dc2f
p3trus <marco.halder@frm2.tum.de>
2015-09-17 14:31:55 +0200

    Added missing numpy import to `fit` module.

--- heatcapacity/fit.py
+++ heatcapacity/fit.py
@@ -3,6 +3,7 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from future.builtins import *
 
+import numpy as np
 from scipy import interpolate, linalg, signal
 
 
