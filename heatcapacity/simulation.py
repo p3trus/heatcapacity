fb1217799930f787e117ca5fce9589ac10cce924
p3trus <marco.halder@frm2.tum.de>
2015-09-17 14:56:28 +0200

    Added missing import.

--- heatcapacity/simulation.py
+++ heatcapacity/simulation.py
@@ -1,3 +1,8 @@
+#  -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from future.builtins import *
+import time
 from heatcapacity.fit import FirstOrder
 
 
