f5a6363ab81bcf43db5cb4223658e2f7422fbe5a
p3trus <marco.halder@frm2.tum.de>
2015-10-07 15:12:35 +0200

    Using audiolazy for the simulation.

--- heatcapacity/simulation.py
+++ heatcapacity/simulation.py
@@ -6,28 +6,32 @@ import time
 
 import numpy as np
 from scipy import signal
+import audiolazy
+
 
 class Simulation(object):
     """The Simulation object can be used if a real experiment is not available.
 
     :param model: An lti model used to simulate the temperature output.
+    :param sampling: The sampling time.
     :param heater_resistance: A resistance in ohm used to calculate the voltage
         drop.
-    :param noise_scale: A sample taken from the standard normal distribution
-        scaled by the `noise_scale` is added to the simulated temperature output.
+    :param sigma: The standart deviation of tjhe gaussian noise added to the simulated temperature
     
 
     E.g.::
 
         import heatcapacity as hc
 
-        sim = hc.Simulation(
-            hc.FirstOrder.from_ck(0.005, 0.002),
-            heater_resistance=1e3,
-            noise_scale=0.1
-        )
         sampling_time = 0.1
-        pulse_sequence = [0.] * 60 * sampling_time + [1.] * 60 * sampling_time
+        sim = hc.Simulation(
+            hc.FirstOrder.from_ck(0.004, 0.002),
+            sampling=sampling_time,
+            heater_resistance=1e3,
+            sigma=1.
+        )
+        
+        pulse_sequence = [0.] * 60 * sampling_time + [0.001] * 60 * sampling_time
 
         measurement = hc.PulsMeasurement(
             currentsource=sim, powermeter=sim, thermometer=sim,
@@ -36,17 +40,26 @@ class Simulation(object):
         measurement.start()
 
     """
-    def __init__(self, model, heater_resistance, noise_scale, sampling_time, x0=0.):
-        # convert model to discrete statespace representation
-        self.model = signal.cont2discrete(signal.tf2ss(model.num, model.den), sampling_time)
-
+    def __init__(self, model, sampling, heater_resistance, sigma, ):
         self.heater_resistance = heater_resistance
-        self.noise_scale = noise_scale
-        self.x0 = x0
-
+        
+        self.power = audiolazy.ControlStream(0.)
         self.current = 0.
-        self._state = None
+        
+        # convert model to discrete representation
+        num, den, dt = signal.cont2discrete((model.num, model.den), sampling)
+        model = audiolazy.ZFilter(list(num.flatten()), list(den.flatten()))
+        self.model = model(self.power) + audiolazy.gauss_noise(sigma=sigma)
 
+    @property
+    def current(self):
+        return self._current
+    
+    @current.setter
+    def current(self, value):
+        self._current = value
+        self.power.value = self.voltage * self.current
+        
     @property
     def voltage(self):
         return self.heater_resistance * self.current
@@ -54,14 +67,4 @@ class Simulation(object):
     @property
     def temperature(self):
         """Simulates the temperature response to the current change."""
-        if self._state is None:
-            u = self.voltage * self.current
-            self._state = u, self.x0
-        u0, x0 = self._state
-        uin = [u0, self.voltage * self.current]
-        tout, yout, xout = signal.dlsim(self.model, uin, x0=x0)
-
-        # Update state
-        self._state = uin[-1], xout[-1]
-
-        return yout[-1] + np.random.normal(scale=self.noise_scale)
+        return self.model.take()
